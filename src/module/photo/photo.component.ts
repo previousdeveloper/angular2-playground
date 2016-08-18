623ed26017dff0828252ea636c9c67816b66277d
previousdeveloper <gokhan.karadas1992@gmail.com>
2016-08-18 23:46:43 +0300

    removeSpacesPipe is added.

--- src/module/photo/photo.component.ts
+++ src/module/photo/photo.component.ts
@@ -1,17 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {JsonPlaceHolderService} from  '../../service/jsonplaceholder/jsonplaceholder.service';
+import {RemoveSpacesPipe} from "../../pipe/removeSpaces.pipe";
 
 @Component({
     moduleId: module.id,
     selector: 'photo',
     templateUrl: 'photo.component.html',
     styleUrls: ['photo.component.css'],
-    providers: [JsonPlaceHolderService]
-
+    providers: [JsonPlaceHolderService],
+    pipes: [RemoveSpacesPipe],
 })
 export class PhotoComponent {
     photos: string[] = null;
     errorMessage;
+    initalValue: number = 20;
 
     constructor(public jsonPlaceHolderService: JsonPlaceHolderService) {
         this.jsonPlaceHolderService = jsonPlaceHolderService;
@@ -29,5 +31,10 @@ export class PhotoComponent {
                 result=>this.photos = result,
                 error => this.errorMessage = <any>error
             )
+    }
+
+    loadMorePhotos() {
+
+        this.initalValue += 10;
     }
 }
--- src/pipe/removeSpaces.pipe.ts
+++ src/pipe/removeSpaces.pipe.ts
@@ -1,3 +1,12 @@
-/**
- * Created by gokhankaradas on 8/18/16.
- */
+import {PipeTransform, Pipe} from "@angular/core";
+
+@Pipe({name: "removeSpace"})
+export class RemoveSpacesPipe implements PipeTransform {
+
+    transform(value: any, args: any): any {
+
+        return value.replace(/ /g, "");
+    }
+
+
+}
